45a9b3cd31b5e5d8f67862f0ac79b3a2b9069aa0
Pratyush Biswas <pratyush.biswas1@gmail.com>
2026-07-14 20:20:41 +0530

    Add worker notes

--- notes/browser/web-workers/worker-main.js
+++ notes/browser/web-workers/worker-main.js
@@ -1,16 +1,52 @@
-//Worker 
-// https://www.greatfrontend.com/questions/quiz/what-are-workers-in-javascript-used-for
+//Worker
+// Workers are used to offload computaion of heavy tasks onto a separate thread from the main thread so that the main thread does not lag while coputing this heavy task.
+// Uses postMessage() and onmessage  event for messaging.
+// Workers have access to various Web APIs, including fetch(), and Web Storage,
+// Workers cannot directly manipulate the DOM
+
+// There are 3 types of workers
 
 // Web Worker
+// Run scripts in background threads, separate from the main UI thread.
+// Useful for CPU-intensive tasks like data processing, calculations, etc.
 
-const myWorker = new Worker("worker.js");
+const worker = new Worker("./worker.js");
 
-myWorker.onmessage = (message) => {
-  console.log(message.data);
-};
+worker.postMessage({ num: 10 });
 
-myWorker.postMessage("Hii Worker");
+worker.onmessage((event) => {
+  console.log(event.data);
+});
+
+worker.onerror((error) => {
+  console.log(error.message);
+});
+
+// Shared Worker
+// Can be accessed by multiple browser tabs/windows from the same origin.
+
+const worker = new SharedWorker("./worker.js");
+
+worker.port.postMessage({ num: 10 });
+
+worker.port.onmessage((event) => {
+  console.log(event.data);
+});
+
+worker.port.onerror((error) => {
+  console.log(error.message);
+});
+
+worker.port.start();
 
 // Service Worker
+// Act as a network proxy between web app, browser, and network.
+// Can intercept and handle network requests, cache resources.
+// Enable offline functionality and push notifications.
 
-navigator.serviceWorker.register("worker.js");
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register()
+    .then((data) => console.log(data))
+    .catch((err) => console.log(err));
+}
--- notes/browser/web-workers/worker.js
+++ notes/browser/web-workers/worker.js
@@ -1,21 +1,52 @@
 // Web Worker
 
-self.onmessage = (message)=>{
-    console.log(message.data);
-}
+onmessage((event) => {
+  const res = myFun(event.data);
+  postMessage(res);
+});
 
-self.postMessage('From Worker');
+const myFun = (data) => {
+  // loop of 1000 length
+  console.log("Heavy computation result");
+};
+
+// Shared Worker
+
+const connections = [];
+
+onconnect((event) => {
+  const port = event.ports[0];
+  connections.push(port);
+
+  port.onmessage((event) => {
+    const res = myFun(event.data);
+    port.postMessage(res);
+  });
+});
+
+const myFun = (data) => {
+  // loop of 1000 length
+  console.log("Heavy computation result");
+};
 
 // Service Worker
 
-self.addEventListener('install',(e)=>{
-
+self.addEventListener("install", () => {
+  // used to fetch the sw and then parse it
 });
 
-self.addEventListener('activate',(e)=>{
-
+self.addEventListener("activate", () => {
+  // Clean old caches
 });
 
-self.addEventListener('fetch',(e)=>{
-    e.respondWith(e.match(e.request));
+self.addEventListener("fetch", (event) => {
+  // it is a proxy that sits between your page and the network for every single request
+  // if the data requrested is present in cache, then serve from cache else hits the api
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      if (response) {
+        return response;
+      } else return fetch(event.request);
+    }),
+  );
 });
